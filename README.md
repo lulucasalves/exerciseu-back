da54ed2b59f17869708fa7d0a36eb3a136e9bdc4
lulucasalves <lucas.alves.supus@outlook.com>
2022-09-08 10:59:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ExerciseU Django
---
+++ exerciseu/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExerciseuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exerciseu'
